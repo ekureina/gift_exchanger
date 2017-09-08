1372286c1b13c680882598ec9f5baba12951b602
Claire Moore <natedevv@gmail.com>
2017-09-07 20:43:24 -0700

    Major Email sending Refactor Only connect to email server once, not every time email is sent. Use stdlib email class to foramat email. Info logging added (can be used for basic debugging).
    Closes #1.

--- gifte_core.py
+++ gifte_core.py
@@ -1,36 +1,29 @@
 #!/usr/bin/env python3
-import random, smtplib
+import random, smtplib, logging
 from collections import namedtuple
+from email.message import EmailMessage
 
 Person = namedtuple('Person', 'name email likes dislikes')
 EMAIL_MESSAGE = "Your TITLE is NAME.\nThey like LIKES.\nThey dislike DISLIKES."
+EMAIL_PORT = 587
+logging.basicConfig(level=logging.INFO)
 
 # Email Sending Function
-def send_email(to, subject, text, credentials, port=587):
-    server = smtplib.SMTP(infer_server_url(credentials[0]), port)
-    server.ehlo()
-    server.starttls()
-    server.login(credentials[0], credentials[1])
-
-    BODY = '\r\n'.join(['To: ' + to,
-                        'From: ' + credentials[0],
-                        'Subject: ' + subject,
-                        '', text])
-
-    try:
-        server.sendmail(email, [to], BODY)
-        print('email sent to ' + to)
-    except BaseException as e:
-        print('error sending mail')
-        print(e)
-
-    server.quit()
+def send_email(recipient, sender, subject, text, server):
+    message = EmailMessage()
+    message.set_content(text)
+    message['Subject'] = subject
+    message['From'] = sender
+    message['To'] = recipient
+    server.send_message(message)
+    logging.info("Sent email to " + recipient)
     
 def infer_server_url(email):
     email_domain = email.split('@')[1]
     return "smtp." + email_domain
 
 def load_preferences(filename):
+    logging.info("Loading Preferences")
     title = ""
     people = [ ]
     NUM_PREFS = 4
@@ -44,6 +37,7 @@ def load_preferences(filename):
     for num_line in range(1, len(lines), NUM_PREFS): # Loop through lines
         people.append(Person(lines[num_line], lines[num_line + 1],
                             lines[num_line+2], lines[num_line + 3]))
+    logging.info("Preferences Loaded")
     return title, people
 
 def gift_designation(people):
@@ -51,25 +45,34 @@ def gift_designation(people):
     giftee = None
     
     gifting = { }
-    
     while len(gifting) < len(people): # While people will still give gifts
         gifter = random.choice(people)
         if gifter not in gifting.keys(): # Gifter hasn't sent yet
             giftee = gifter
-            while giftee == gifter or giftee not in gifting.values(): # No duplicates
-                giftee = random.choice(names)
+            while giftee == gifter or giftee in gifting.values(): # No duplicates
+                giftee = random.choice(people)
         gifting[gifter] = giftee
+    logging.info("Gifts designated.")
+    return gifting
     
 def send_emails(gifting, title, credentials):
     subject = "Welcome to " + title
+    # Login to our SMTP server
+    server = smtplib.SMTP(infer_server_url(credentials[0]), EMAIL_PORT)
+    server.starttls()
+    server.login(credentials[0], credentials[1])
+    logging.info("Started email connection")
     for gifter in gifting.keys():
         giftee = gifting[gifter]
-        send_email(gifter.email, subject, personalize_message(title, giftee),
-            credentials)
+        send_email(gifter.email, credentials[0], subject, personalize_message(title, giftee), server)
+        
+    # Logout from the SMTP server when done with emails
+    logging.info("Finished sending emails")
+    server.quit()
         
 def personalize_message(title, person):
     message = EMAIL_MESSAGE.replace("TITLE", title)
     message = message.replace("NAME", person.name)
-    message = message.replace("LIKES", person.likes)
+    message = message.replace(" LIKES", " " + person.likes)
     message = message.replace("DISLIKES", person.dislikes)
     return message
